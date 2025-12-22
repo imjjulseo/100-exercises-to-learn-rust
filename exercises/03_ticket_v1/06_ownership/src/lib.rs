a449763529b771e43a6bfb7e8580353f248facaf
imjjulseo <imjjulseo@snu.ac.kr>
2025-12-22 14:55:48 +0000

    ownership

--- exercises/03_ticket_v1/06_ownership/src/lib.rs
+++ exercises/03_ticket_v1/06_ownership/src/lib.rs
@@ -34,16 +34,16 @@ impl Ticket {
         }
     }
 
-    pub fn title(self) -> String {
-        self.title
+    pub fn title(&self) -> &String {
+        &self.title
     }
 
-    pub fn description(self) -> String {
-        self.description
+    pub fn description(&self) -> &String {
+        &self.description
     }
 
-    pub fn status(self) -> String {
-        self.status
+    pub fn status(&self) -> &String {
+        &self.status
     }
 }
 
@@ -57,8 +57,8 @@ mod tests {
         // If you change the signatures as requested, this should compile:
         // we can call these methods one after the other because they borrow `self`
         // rather than taking ownership of it.
-        assert_eq!(ticket.title(), "A title");
-        assert_eq!(ticket.description(), "A description");
-        assert_eq!(ticket.status(), "To-Do");
+        assert_eq!(&ticket.title(), "A title");
+        assert_eq!(&ticket.description(), "A description");
+        assert_eq!(&ticket.status(), "To-Do");
     }
 }
